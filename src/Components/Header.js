7409ee528b9adb37145c6a82b03df69776d2efb4
Ricky Zaman <ricky.zaman@outlook.com>
2021-08-18 22:41:53 -0400

    added header icons and styling

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,8 +1,22 @@
 import '../styles/Header.css';
+import PersonIcon from '@material-ui/icons/Person';
+import IconButton from '@material-ui/core/IconButton';
+import ForumIcon from '@material-ui/icons/Forum';
+
 const Header = () => {
     return (
-        <div>
-            <h1 className="header">MBA</h1>
+        <div className="header">
+            <IconButton>
+                <PersonIcon fontSize="large" className="header__icon"/>
+            </IconButton>
+            <img
+            className="header__logo"
+            src= "/tinder.png"
+            alt="Tinder logo"
+            />
+            <IconButton>
+                <ForumIcon fontSize="large" className="header__icon"/>
+            </IconButton>
         </div>
     )
 }
