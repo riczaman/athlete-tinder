715c31fd3696e29d259f8ee892dc753884de7ada
Ricky Zaman <ricky.zaman@outlook.com>
2021-08-29 20:05:49 -0400

    connected frontend with backend

--- src/Components/TinderCards.js
+++ src/Components/TinderCards.js
@@ -1,19 +1,27 @@
 import '../styles/TinderCards.css';
 import TinderCard from 'react-tinder-card'
-import {useState} from 'react'
+import {useState, useEffect} from 'react'
 
 const TinderCards = () => {
     //will pull from the backend after using fetch 
-    const [athletes, setAthletes] = useState([
-        {
-            name: "Candace Parker",
-            url: "https://cdn.vox-cdn.com/thumbor/RPKQvi1KXlagAkM72CzM-AXv7fU=/0x0:1996x3000/1200x800/filters:focal(819x247:1137x565)/cdn.vox-cdn.com/uploads/chorus_image/image/61851569/984144892.jpg.0.jpg"
-        },
-        {
-            name: "Paige Bueckers", 
-            url: "https://cdn.cnn.com/cnnnext/dam/assets/210711101757-restricted-paige-bueckers-07-10-2021-exlarge-169.jpg"
-        }
-    ]);
+    const [athletes, setAthletes] = useState([]);
+
+    //pull random 4-6 until they refresh
+    //refresh new 4 
+    //heart = swipe right 
+    //x swipe left 
+    //add element or div that appears when they dnt have anymore cards
+    
+    useEffect(()=>{
+        getTinderProfiles(); 
+    }, [])
+
+    const getTinderProfiles = async() =>{
+        const res = await fetch("http://localhost:8000/tinder/card"); 
+        const data = await res.json();
+
+        setAthletes(data); 
+    }
 
     const swiped = (direction, name) =>{
         console.log(`You swiped ${direction} on ${name}`)
@@ -33,7 +41,7 @@ const TinderCards = () => {
                     preventSwipe={['up', 'down']}
                     onSwipe={(dir)=>swiped(dir, athlete.name)}
                     onCardLeftScreen={()=> outOfFrame(athlete.name)}>
-                    <div style={{ backgroundImage: `url('${athlete.url}')` }} className="card">
+                    <div style={{ backgroundImage: `url('${athlete.imgUrl}')` }} className="card">
                         <h3>{athlete.name}</h3>
                     </div>
                 </TinderCard>)}
